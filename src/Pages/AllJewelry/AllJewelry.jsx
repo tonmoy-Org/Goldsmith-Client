abd26a8f4bb913f1b61587882b5fa763e928c4af
tonmoy-Org <tonmoy15-5200@diu.edu.bd>
2023-10-12 12:33:09 +0600

    Routes added and added main layout also landing pages

--- src/Pages/AllJewelry/AllJewelry.jsx
+++ src/Pages/AllJewelry/AllJewelry.jsx
@@ -1,6 +1,6 @@
 
 
-const Banner = () => {
+const AllJewelry = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Banner = () => {
     );
 };
 
-export default Banner;
+export default AllJewelry;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-purple-500">This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
